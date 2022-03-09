73767cbdb7fcac050748cae93682315d24b52bd6
TomaszTrojanski <79982724+TomaszTrojanski@users.noreply.github.com>
2022-03-09 18:28:06 +0100

    działa wow

--- zajecia1/index.ts
+++ zajecia1/index.ts
@@ -1,6 +1,23 @@
 const express = require('express')  
 const app = express()  
 app.get('/', function (req, res) {  
-  res.send('Hello World')})
+  let operator : string = req.query.operator;
+  let num1 : number = +req.query.num1;
+  let num2 : number = +req.query.num2;
+  let result : number = null;
+
+  if(operator=="dod")
+    result=num1+num2;
+  else if(operator=="ode")
+    result=num1-num2;
+  else if(operator=="mno")
+    result=num1*num2;
+  else if(operator=="pod")
+    result=num1/num2;
   
+res.send(result.toString());
+
+
+})
+
 app.listen(3000)  
