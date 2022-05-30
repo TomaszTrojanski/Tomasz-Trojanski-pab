183258be644dc00fb696c228971d8c5824dcf2ca
TomaszTrojanski <79982724+TomaszTrojanski@users.noreply.github.com>
2022-05-31 01:32:20 +0200

    Employee model create

---
+++ ProjektRestauracja/index.ts
@@ -0,0 +1,12 @@
+class Employee{
+    name: string;
+    surname: string;
+    position: string;
+
+    constructor(name: string, surname: string, position: string){
+        this.name = name;
+        this.surname = surname;
+        this.position = position;
+    }
+}
+export default Employee;
