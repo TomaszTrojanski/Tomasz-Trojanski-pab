80eaf2a572819df8eae620fb375ec3a79a2b1fa6
TomaszTrojanski <79982724+TomaszTrojanski@users.noreply.github.com>
2022-05-31 19:49:35 +0200

    udoskonalenie indexu

---
+++ ProjektRestauracja/index.ts
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Restaurant = require('./CoreBusiness/RestaurantModel');
+import express = require ('express');
+import {Request, Response} from 'express';
+import { RestaurantRepository } from "./DataStore/RestaurantRepository";
+
+const app = express();
+const router = express.Router();
+const restaurantRepository = new RestaurantRepository();
+router.get('/restaurants', async (req: Request, res: Response) => {
+    let restaurants = await restaurantRepository.getRestaurant();
+    if (restaurants) {
+        res.json(restaurants);
+    } else {
+        res.status(404).send("No restaurants found");
+    }
+});
+router.get('/restaurant/:name', async (req: Request, res: Response) => {
+    let restaurant = await restaurantRepository.getRestaurantByName(req.params.name);
+    if (restaurant)
+    {
+        res.json(restaurant);
+    }
+    else
+    {
+        res.status(404).send("Restaurant not found");
+    }
+});
+app.use('/', router);
+app.listen(3000);
