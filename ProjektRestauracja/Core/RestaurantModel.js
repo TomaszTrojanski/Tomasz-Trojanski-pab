81bd103d600596549a0aa0035bf84b01f0421a90
TomaszTrojanski <79982724+TomaszTrojanski@users.noreply.github.com>
2022-05-31 20:37:24 +0200

    automat JS

---
+++ ProjektRestauracja/Core/RestaurantModel.js
@@ -0,0 +1,15 @@
+"use strict";
+exports.__esModule = true;
+var Restaurant = /** @class */ (function () {
+    function Restaurant(name, address, phone, nip, email, website, description) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.nip = nip;
+        this.email = email;
+        this.website = website;
+        this.description = description;
+    }
+    return Restaurant;
+}());
+exports["default"] = Restaurant;
