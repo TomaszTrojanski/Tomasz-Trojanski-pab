09b7f1f28987e266ac11e18e158100726c9323b7
TomaszTrojanski <79982724+TomaszTrojanski@users.noreply.github.com>
2022-05-31 19:02:42 +0200

    przpyisanie pracownika do restauracji

--- ProjektRestauracja/Core/EmployeeModel.ts
+++ ProjektRestauracja/Core/EmployeeModel.ts
@@ -1,12 +1,16 @@
+import Restaurant from "./RestaurantModel";
+
 class Employee{
     name: string;
     surname: string;
     position: string;
+    restaraunt: Restaurant;
 
-    constructor(name: string, surname: string, position: string){
+    constructor(name: string, surname: string, position: string, restaraunt: Restaurant){
         this.name = name;
         this.surname = surname;
         this.position = position;
+        this.restaraunt = restaraunt;
     }
 }
 export default Employee;
