3d3d62db31bd58b697fb246f8c71ab48ca6edef5
TomaszTrojanski <79982724+TomaszTrojanski@users.noreply.github.com>
2022-05-31 02:09:13 +0200

    Reservation model create

---
+++ ProjektRestauracja/Models/ReservationModel.ts
@@ -0,0 +1,18 @@
+import Table from "./TablesModel";
+import Customer from "./CustomerModel";
+
+class Reservation{
+    table: Table;
+    startDateTime: Date;
+    endDateTime: Date;
+    customer: Customer;
+
+    constructor(table: Table, startDateTime: Date, endDateTime: Date, customer: Customer){
+        this.table = table;
+        this.startDateTime = startDateTime;
+        this.endDateTime = endDateTime;
+        this.customer = customer;
+    }
+}
+export default Reservation;
+
