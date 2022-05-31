cd2e241705caf4ac318314881f61c980b7916655
TomaszTrojanski <79982724+TomaszTrojanski@users.noreply.github.com>
2022-05-31 16:25:22 +0200

    RestaurantModel add description

--- ProjektRestauracja/Models/RestaurantModel.ts
+++ ProjektRestauracja/Models/RestaurantModel.ts
@@ -5,14 +5,16 @@ class Restaurant{
     nip: string;
     email: string;
     website: string;
+    description?: string;
 
-    constructor(name: string, address: string, phone: string, nip: string, email: string, website: string){
+    constructor(name: string, address: string, phone: string, nip: string, email: string, website: string, description?: string;){
         this.name = name;
         this.address = address;
         this.phone = phone;
         this.nip = nip;
         this.email = email;
         this.website = website;
+        this.description = description;
     }
 }
 export default Restaurant;
