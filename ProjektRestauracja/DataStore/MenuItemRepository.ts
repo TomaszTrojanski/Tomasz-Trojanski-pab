95add0658c44ab9b12b0a12f9661618c5ddabaef
TomaszTrojanski <79982724+TomaszTrojanski@users.noreply.github.com>
2022-06-01 05:48:26 +0200

    duże zmiany menuitemrepo

--- ProjektRestauracja/DataStore/MenuItemRepository.ts
+++ ProjektRestauracja/DataStore/MenuItemRepository.ts
@@ -1,15 +1,175 @@
 import { Schema,model, connect } from "mongoose";
 import MenuItem from "../Core/MenuItemModel";
-import Product from "../Core/ProductModel";
 
 export class MenuItemRepository{
     menuItemSchema = new Schema<MenuItem>({
         menuItemId : {type: Number, required: true},
         name: {type: String, required: true},
         price: {type: Number, required: true},
-        type: {type: String, required: true},
+        type: {type: Number, required: true},
         description: {type: String, required: true},
-        product: {type: [Product], required: true}
+        product: {type: Schema.Types.ObjectId, ref: 'Product', required: true}
     });
     MenuItemModel = model<MenuItem>('MenuItem', this.menuItemSchema);
+
+    async populateMenuItems() : Promise<void>{
+        await connect('mongodb+srv://Admin:<AdminAdmin>@cluster0.tpgqv.mongodb.net/?retryWrites=true&w=majority');
+
+        const menuItems =
+        [
+            {
+                name: 'Coca_Cola',
+                price: 5,
+                type: 3,
+                description: 'Coca Cola can',
+                products: 
+                [
+                        '6283c1db0ff7f2140b54e984'
+                ]
+            },
+            {
+                name: 'Fanta',
+                price: 5,
+                type: 3,
+                description: 'Fanta can',
+                products: 
+                [
+                    '6283c1db0ff7f2140b54e985'
+                ]
+            },
+            {
+                name: 'Vegetable soup',
+                price: 15,
+                type: 2,
+                description: 'Vegetable soup made of carrot, parsley, onion, tomato and cucumber',
+                products: 
+                [
+                    '6283c1db0ff7f2140b54e986',
+                    '6283c1db0ff7f2140b54e987',
+                    '6283c1db0ff7f2140b54e988',
+                    '6283c1db0ff7f2140b54e989',
+                    '6283c1db0ff7f2140b54e98a'
+                ]
+            },
+            {
+                name: 'Red Wine',
+                price: 15,
+                type: 4,
+                description: 'Red wine',
+                products: 
+                [
+                    '6283c1db0ff7f2140b54e986',
+                    '6283c1db0ff7f2140b54e987',
+                    '6283c1db0ff7f2140b54e988',
+                    '6283c1db0ff7f2140b54e989',
+                    '6283c1db0ff7f2140b54e98a'
+                ]
+            },
+            {
+                name: 'Red Wine',
+                price: 15,
+                type: 4,
+                description: 'Red wine',
+                products: 
+                [
+                    '6283c2b5ed64cb8ae6973cdb'
+                ]
+            }
+        ];
+
+        if(await this.MenuItemModel.countDocuments() === 0)
+        {
+            await this.MenuItemModel
+            .insertMany(menuItems)
+            .then(function()
+            {
+                console.log('Menu items populated');
+            }).catch(function(err: any)
+            {
+                console.log(err);
+            });
+        }
+    }
+
+    async addMenuItem(menuItem: MenuItem) : Promise<void>
+    {
+        await connect('mongodb+srv://Admin:<AdminAdmin>@cluster0.tpgqv.mongodb.net/?retryWrites=true&w=majority');
+        await this.MenuItemModel
+        .create(menuItem)
+        .then(function()
+        {
+            console.log('Menu item ' + menuItem.name + ' has been added!');
+        }).catch(function(err: any)
+        {
+            console.log(err);
+        });
+    }
+
+    async deleteMenuItemByName(menuItem: string) : Promise<void>
+    {
+        await connect('mongodb+srv://Admin:<AdminAdmin>@cluster0.tpgqv.mongodb.net/?retryWrites=true&w=majority');
+        await this.MenuItemModel
+        .deleteOne({name: menuItem})
+        .then(function()
+        {
+            console.log('Menu item ' + menuItem + ' has been deleted!');
+        }).catch(function(err: any)
+        {
+            console.log(err);
+        });
+
+    }
+
+    async getMenuItemByName(menuItemName: string) : Promise<MenuItem>
+    {
+        await connect('mongodb+srv://Admin:<AdminAdmin>@cluster0.tpgqv.mongodb.net/?retryWrites=true&w=majority');
+        let menuItem = await this.MenuItemModel.findOne({name: menuItemName});
+
+        if(menuItem)
+        {
+            return menuItem;
+        }
+        else
+        {
+            console.log("Menu item " + menuItemName + " not found!");
+            return null as any;
+        }
+    }
+
+    async getMenuItems() : Promise<MenuItem[]>
+    {
+        await connect('mongodb+srv://Admin:<AdminAdmin>@cluster0.tpgqv.mongodb.net/?retryWrites=true&w=majority');
+
+        return await this.MenuItemModel.find();
+    }
+    async updateMenuItem(menuItemName: string,menuItem:MenuItem) : Promise<void>{
+        await connect('mongodb+srv://Admin:<AdminAdmin>@cluster0.tpgqv.mongodb.net/?retryWrites=true&w=majority');
+        let menuItemToUpdate = await this.MenuItemModel.findOne({name: menuItemName});
+
+        if(menuItemToUpdate)
+        {
+            if(menuItem.name)
+                menuItemToUpdate.name = menuItem.name;
+            if(menuItem.price)
+                menuItemToUpdate.price = menuItem.price;
+            if(menuItem.type)
+                menuItemToUpdate.type = menuItem.type;
+            if(menuItem.description)
+                menuItemToUpdate.description = menuItem.description;
+            if(menuItem.products)
+                menuItemToUpdate.products = menuItem.products;
+
+            await menuItemToUpdate.save()
+            .then(function()
+            {
+                console.log('Menu item ' + menuItemName + ' has been updated!');
+            }).catch(function(err: any)
+            {
+                console.log(err);
+            });
+        }
+        else {
+            console.log('Menu item ' + menuItemName + ' does not exist!');
+        }
+    }
 }
