3902a0ae98c5abfabc8a66f67cc2b49ab4cb080a
TomaszTrojanski <79982724+TomaszTrojanski@users.noreply.github.com>
2022-06-01 03:21:00 +0200

    EmployeeRepo dużo zmian

--- ProjektRestauracja/DataStore/EmployeeRepository.ts
+++ ProjektRestauracja/DataStore/EmployeeRepository.ts
@@ -1,72 +1,211 @@
-import { Schema,model,connect } from "mongoose";
-import Employee from "../Core/EmployeeModel";
-import { RestaurantRepository } from "./RestaurantRepository";
+import {Schema, model, connect} from 'mongoose';
+import Employee from '../Core/EmployeeModel';
+import Restaurant from '../Core/RestaurantModel';
 
 export class EmployeeRepository
 {
-    employeeSchema = new Schema<Employee>({
-        employeeId: {type: Number, required: true},
-        name: {type: String, required: true},
-        surname: {type: String, required: true},
-        position: {type: String, required: true},
-        restaurantName: {type: String, required: true}
-    });
+    restaurantSchema = new Schema<Restaurant>(
+        {
+            name: {type: String, required: true},
+            address: {type: String, required: true},
+            phone: {type: String, required: true},
+            nip: {type: String, required: true},
+            email: {type: String, required: true},
+            website: {type: String, required: true},
+            description: {type: String, required: false}
+        });
+
+
+    employeeSchema = new Schema<Employee>(
+        {
+            name: {type: String, required: true},
+            surname: {type: String, required: true},
+            position: {type: String, required: true},
+            restaurant: {type: this.restaurantSchema, required: true}
+        });
+
     EmployeeModel = model<Employee>('Employee', this.employeeSchema);
 
     async populateEmployees() : Promise<void>
     {
         await connect('mongodb+srv://Admin:<AdminAdmin>@cluster0.tpgqv.mongodb.net/?retryWrites=true&w=majority');
 
-        const employees =[
+        const employees =
+        [
             {
-                employeeId: 1,
-                name: "Jan",
-                surname: "Kowalski",
+                name: "Employee1",
+                surname: "Employee1",
                 position: "Manager",
-                restaurantName: "Restauracja"
-        },{
-                employeeId: 2,
-                name: "Adam",
-                surname: "Nowak",
+                restaurant: 
+                {
+                    name: "Restaurant1",
+                    address: "Address1",
+                    phone: "123456789",
+                    nip: "123456789",
+                    email: "someEmail@something.com",
+                    website: "someWebsite.com"
+                }
+            },
+            {
+                name: "Employee2",
+                surname: "Employee2",
                 position: "Waiter",
-                restaurantName: "Restauracja"
-        },{
-                employeeId: 3,
-                name: "Piotr",
-                surname: "Kowalski",
+                restaurant: 
+                {
+                    name: "Restaurant1",
+                    address: "Address1",
+                    phone: "123456789",
+                    nip: "123456789",
+                    email: "someEmail@something.com",
+                    website: "someWebsite.com"
+                }
+            },
+            {
+                name: "Employee3",
+                surname: "Employee3",
                 position: "Waiter",
-                restaurantName: "Restauracja2"
-        },{
-                employeeId: 4,
-                name: "Krzysztof",
-                surname: "Kowalski",
-                position: "Waiter",
-                restaurantName: "Restauracja2"
-        }];
-        await this.EmployeeModel
-        .insertMany(employees)
-        .then(function(){
-            console.log('Employees have been populated')
-        }).catch(function(err){
-            console.log(err);
-        });
+                restaurant: 
+                {
+                    name: "Restaurant1",
+                    address: "Address1",
+                    phone: "123456789",
+                    nip: "123456789",
+                    email: "someEmail@something.com",
+                    website: "someWebsite.com"
+                }
+            },
+            {
+                name: "Employee4",
+                surname: "Employee4",
+                position: "Chef",
+                restaurant: 
+                {
+                    name: "Restaurant1",
+                    address: "Address1",
+                    phone: "123456789",
+                    nip: "123456789",
+                    email: "someEmail@something.com",
+                    website: "someWebsite.com"
+                }
+            }
+        ];
+
+        if(await this.EmployeeModel.countDocuments() === 0)
+        {
+            await this.EmployeeModel
+            .insertMany(employees)
+            .then(function()
+            {
+                console.log("Employees have been populated!")
+            }).catch(function(err)
+            {
+                console.log(err);
+            });
+        }
     }
-    async addEmployee(employee: Employee):Promise<void>
+
+    async addEmployee(employee: Employee) : Promise<boolean>
     {
         await connect('mongodb+srv://Admin:<AdminAdmin>@cluster0.tpgqv.mongodb.net/?retryWrites=true&w=majority');
 
-        let restaurantRepository = new RestaurantRepository();
-        let restaurantExists: boolean = await restaurantRepository.checkIfRestaurantExists(employee.restaurantName);
+        const alreadyExists = await this.EmployeeModel.findOne({name: employee.name, surname: employee.surname});
+        if(alreadyExists)
+            return false;
 
-        if(restaurantExists){
-            await this.EmployeeModel
-            .create(employee)
-            .then(function(){
-                console.log("Employee"+employee.name+"has been added")}
-            ).catch(function(err){
+        await this.EmployeeModel
+        .create(employee)
+        .then(function()
+        {
+            console.log("Employee " + employee.name + " " + employee.surname + " has been added!");
+        }).catch(function(err)
+        {
+            console.log(err);
+        });
+
+        const existsAfter = await this.EmployeeModel.findOne({surname: employee.surname});
+        if(existsAfter)
+            return true;
+        else
+            return false;
+    }
+
+    async deleteEmployeeBySurnameAndName(employeeSurname: string, employeeName: string) : Promise<boolean>
+    {
+        await connect('mongodb+srv://Admin:<AdminAdmin>@cluster0.tpgqv.mongodb.net/?retryWrites=true&w=majority');
+
+        const exists = await this.EmployeeModel.findOne({surname: employeeSurname, name: employeeName});
+        if(!exists)
+            return false;
+
+        await this.EmployeeModel
+        .deleteOne({name: employeeName, surname: employeeSurname})
+        .then(function()
+        {
+            console.log("Employee " + employeeName + " " + employeeSurname + " has been deleted!");
+        }).catch(function(err)
+        {
+            console.log(err);
+        });
+
+        const existsAfter = await this.EmployeeModel.findOne({name: employeeName, surname: employeeSurname});
+        if(!existsAfter)
+            return true;
+        else
+            return false;
+    }
+
+    async getEmployeesBySurname(employeeSurname: string) : Promise<Employee[] | boolean>
+    {
+        await connect('mongodb+srv://Admin:<AdminAdmin>@cluster0.tpgqv.mongodb.net/?retryWrites=true&w=majority');
+
+        const employees = await this.EmployeeModel.find({surname: employeeSurname});
+        if(employees.length > 0)
+            return employees;
+        else
+            return false;
+    }
+
+    async getEmployees() : Promise<Employee[] | boolean>
+    {
+        await connect('mongodb+srv://Admin:<AdminAdmin>@cluster0.tpgqv.mongodb.net/?retryWrites=true&w=majority');
+
+        const employees = await this.EmployeeModel.find({});
+        if(employees. length > 0)
+            return employees;
+        else
+            return false;
+    }
+
+    async updateEmployeeBySurnameAndName(employeeSurname: string, employeeName:string, employee: Employee) : Promise<boolean>
+    {
+        await connect('mongodb+srv://Admin:<AdminAdmin>@cluster0.tpgqv.mongodb.net/?retryWrites=true&w=majority');
+
+        
+
+        let employeeToUpdate = await this.EmployeeModel.findOne({surname: employeeSurname, name: employeeName});
+        if(employeeToUpdate)
+        {
+            if(employee.name)
+             employeeToUpdate.name = employee.name;
+            if(employee.surname)
+                employeeToUpdate.surname = employee.surname;
+            if(employee.position)
+                employeeToUpdate.position = employee.position;
+            if(employee.restaurant)
+                employeeToUpdate.restaurant = employee.restaurant;
+    
+            await employeeToUpdate.save()
+            .then(function()
+            {
+                console.log("Employee " + employee.surname + " " + employee.name + " has been updated!");
+            }).catch(function(err)
+            {
                 console.log(err);
             });
-        }else{
-            console.log("Restaurant"+employee.restarauntName+ "does not exist");
+
+            return true;
         }
+        else 
+            return false;
+    }
 }
