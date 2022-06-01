e6319349a5045bfe70a6234552a52033c77adc73
TomaszTrojanski <79982724+TomaszTrojanski@users.noreply.github.com>
2022-06-01 02:48:35 +0200

    zmiany index

--- ProjektRestauracja/index.ts
+++ ProjektRestauracja/index.ts
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose');
-const [Restaurant] = require('./CoreBusiness/RestaurantModel');
+const {Customer} = require('./Core/CustomerModel');
+const {Employee} = require('./Core/EmployeeModel');
+const {MenuItem} = require ('./Core/MenuItemModel');
+const {Order} = require('./Core/OrderModel');
+const {OrderItem} = require('./Core/OrderItemModel');
+const {Product} = require('./Core/ProductModel');
+const {Reservation} = require('./Core/ReservationModel');
+const {Restaurant} = require('./Core/RestaurantModel');
+const {Table} = require('./Core/TablesModel');
 import bodyParser from 'body-parser';
 import express from 'express';
 import {Request, Response} from 'express';
-import { RestaurantRepository } from "./DataStore/RestaurantRepository";
+import { CustomerRepository } from './DataStore/CustomreRepository';
+import { EmployeeRepository } from './DataStore/EmployeeRepository';
+import { MenuItemRepository } from './DataStore/MenuItemRepository';
+import { ProductRepository } from './DataStore/ProductRepository';
+import { RestaurantRepository } from './DataStore/RestaurantRepository';
+import { ReservationRepository } from './DataStore/ReservationRepository';
+import { TableRepository } from './DataStore/TableRepository';
 
 const app = express();
 const router = express.Router();
@@ -12,17 +26,60 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', router);
 
+const customerRepository = new CustomerRepository();
+const employeeRepository = new EmployeeRepository();
+const menuItemRepository = new MenuItemRepository();
+const productRepository = new ProductRepository();
+const reservationRepository = new ReservationRepository();
 const restaurantRepository = new RestaurantRepository();
+const tableRepository = new TableRepository();
+// REST API for Customer
+// get all customers
+router.get('/customers', async (req: Request, res: Response) => {
+    const customers = await customerRepository.getCustomers();
+    if (customers.length > 0) 
+        res.status(200).json(customers);
+    else if(customers.length === 0)
+        res.status(404).json({message: 'Customers list is empty'});
+    else
+        res.status(404).json({message: 'No customer list found'});
+});
+// get customer by name
+router.get('/customers/:name', async (req: Request, res: Response) => {
+    const customer = await customerRepository.getCustomerByName(req.params.name);
+    if (customer)
+        res.status(200).json(customer);
+    else
+        res.status(404).json({message: 'Customer ' + req.params.name + ' not found'});
+});
+// delete customer by name
+router.delete('/customers/:name', async (req: Request, res: Response) => {
+    const customer = await customerRepository.deleteCustomerByName(req.params.name);
+    res.status(200).json('Restaurant ' + req.params.name + ' deleted');
+});
+// add customer from body
+router.post('/customers', async (req: Request, res: Response) => {
+    const customer = await customerRepository.addCustomer(req.body);
+    res.status(200).json(customer);
+});
+// update customer from body
+router.put('/customers/:name', async (req: Request, res: Response) => {
+    const customer = await customerRepository.updateCustomer(req.body);
+    res.status(200).json(customer);
+});
+// REST API for Restaurant
+// get all restaurants
 router.get('/restaurants', async (req: Request, res: Response) => {
     let restaurants = await restaurantRepository.getRestaurant();
     if (restaurants.length>0) {
         res.json(restaurants);
-    } else if(restaurants.length==0) {
-        res.status(200).send('Restaurants list is empty');
-    } else {
-        res.status(404).send("No restaurants found");
+    } else if (restaurants.length === 0)
+        res.status(200).send('Restaurant list is empty');
+    else
+        res.status(404).send('No restaurants found');
     }
 });
+// get restaurant by name
 router.get('/restaurant/:name', async (req: Request, res: Response) => {
     const restaurant = await restaurantRepository.getRestaurantByName(req.params.name);
     if (restaurant)
@@ -31,16 +88,18 @@ router.get('/restaurant/:name', async (req: Request, res: Response) => {
     }
     else
     {
-        res.status(404).send("Restaurant not found");
+        res.status(404).send('Restaurant not found');
     }
 });
+// delete restaurant by name
 router.delete('/restaurant/:name', async (req: Request, res: Response) => {
     await restaurantRepository.deleteRestaurantByName(req.params.name);
-    res.status(200).send("Restaurant deleted");
+    res.status(200).send('Restaurant deleted');
 });
+// add restaurant from body
 router.post('/restaurant', async (req: Request, res: Response) => {
     const restaurant = req.body;
     await restaurantRepository.addRestaurant(restaurant);
-    res.status(200).send("Restaurant added");
+    res.status(200).send('Restaurant added');
 });
 app.listen(3000);
